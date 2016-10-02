98c5bbab96f9d12470bc0372b607059cfc545bd2
fjohnson1991 <felicity.johnson.mail@gmail.com>
2016-10-01 22:23:08 -0400

    commit

--- StructLove/Tiger.swift
+++ StructLove/Tiger.swift
@@ -8,3 +8,21 @@
 
 import Foundation
 
+struct Tiger {
+    
+    var name : String
+    var isHungry : Bool = false
+    
+    init(name: String) {
+        
+        self.name = name
+        
+    }
+    
+    mutating func eat() {
+        
+        isHungry = false
+    }
+    
+    
+}
